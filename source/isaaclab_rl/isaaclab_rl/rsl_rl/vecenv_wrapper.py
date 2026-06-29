b6f8f39850626af581bfd5e568d652939e06e6a6
Robert <rjgriffin42@gmail.com>
2026-06-29 12:53:08 -0500

    added addition fields to rsl env

--- source/isaaclab_rl/isaaclab_rl/rsl_rl/vecenv_wrapper.py
+++ source/isaaclab_rl/isaaclab_rl/rsl_rl/vecenv_wrapper.py
@@ -59,6 +59,20 @@ class RslRlVecEnvWrapper(VecEnv):
             self.num_actions = self.unwrapped.action_manager.total_action_dim
         else:
             self.num_actions = gym.spaces.flatdim(self.unwrapped.single_action_space)
+        if hasattr(self.unwrapped, "observation_manager"):
+            self.num_obs = self.unwrapped.observation_manager.group_obs_dim["policy"][0]
+        else:
+            self.num_obs = gym.spaces.flatdim(self.unwrapped.single_observation_space["policy"])
+        # -- privileged observations
+        if (
+            hasattr(self.unwrapped, "observation_manager")
+            and "critic" in self.unwrapped.observation_manager.group_obs_dim
+        ):
+            self.num_privileged_obs = self.unwrapped.observation_manager.group_obs_dim["critic"][0]
+        elif hasattr(self.unwrapped, "num_states") and "critic" in self.unwrapped.single_observation_space:
+            self.num_privileged_obs = gym.spaces.flatdim(self.unwrapped.single_observation_space["critic"])
+        else:
+            self.num_privileged_obs = 0
 
         # modify the action space to the clip range
         self._modify_action_space()
